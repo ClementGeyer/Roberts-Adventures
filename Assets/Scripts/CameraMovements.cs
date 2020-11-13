27c2eee157f21739911ae6635acf694801d18c4f
Elouan Haquin <elouan.haquin@etu.unistra.fr>
2020-11-13 15:41:20 +0100

    Ajout de la base du projet Unity

---
+++ Assets/Scripts/CameraMovements.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovements : MonoBehaviour
+{
+    public GameObject hero;
+    public float offsetCamera = 10.0f;
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+         rb = this.GetComponent<Rigidbody2D>();
+         
+    }
+
+   
+    void FixedUpdate()
+    {
+        //if( this.GetComponent<Transform>().position.x - offsetCamera < hero.GetComponent<Transform>().position.x ){
+           rb.velocity = new Vector3(hero.GetComponent<Rigidbody2D>().velocity.x, 0 ,-10);
+        
+        
+    }
+}
---
+++ Assets/Scripts/Movements.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movements : MonoBehaviour
+{
+
+   // public GameObject Ga;
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb =  this.GetComponent<Rigidbody2D>();
+    
+    }
+
+
+    void FixedUpdate()
+    {
+        if(rb.velocity.x < 5){
+            rb.velocity = new Vector3(rb.velocity.x + 1, rb.velocity.y);
+        }
+
+        if(Input.GetAxis("Vertical")>0 &&  rb.velocity.y < 5) {
+
+           rb.velocity = new Vector3(rb.velocity.x , rb.velocity.y + 3);
+        }
+    }
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        //Lost todo check col tag name
+       // Destroy(this);
+    }
+}
