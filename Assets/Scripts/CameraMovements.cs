79dd214983aa8c65f9c3e0114ff2edfd92e2119d
Elouan Haquin <elouan.haquin@etu.unistra.fr>
2020-11-15 18:16:05 +0100

    Changement du type de travelling de la camera

--- Assets/Scripts/CameraMovements.cs
+++ Assets/Scripts/CameraMovements.cs
@@ -4,8 +4,12 @@ using UnityEngine;
 
 public class CameraMovements : MonoBehaviour
 {
+    [Header("Camera parameters")]
+    [SerializeField] private float offsetCamera = 0.0f;
+    [SerializeField] private float speedCamera = 0.05f;
+    [SerializeField] private float increaseSpeedCamera = 0.05f;
+    
     public GameObject hero;
-    public float offsetCamera = 10.0f;
     private Transform tf;
 
     void Start()
@@ -17,8 +21,12 @@ public class CameraMovements : MonoBehaviour
    
     void FixedUpdate()
     {
+        
+        speedCamera *= 1+ increaseSpeedCamera;
+        tf.position = new Vector3(  tf.position.x + speedCamera, tf.position.y,  tf.position.z);
+
         if( tf.position.x < hero.GetComponent<Transform>().position.x )
-           tf.position = new Vector3(   hero.GetComponent<Transform>().position.x, tf.position.y,  tf.position.z);
+           tf.position = new Vector3( hero.GetComponent<Transform>().position.x, tf.position.y,  tf.position.z);
         
         
     }
