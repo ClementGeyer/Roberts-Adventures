16775f5fddfb69f8eb7b97fb65d181a5326bf140
EDEL GAUTIER <gautier.edel@etu.unistra.fr>
2020-11-18 15:51:09 +0100

    Finition du premier niveau

---
+++ Assets/Scripts/Map/DestroyMapPart.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class DestroyMapPart : MonoBehaviour
+{
+
+    // Lorsque une partie de la map sort de le camera
+    void OnBecameInvisible()
+    {
+        //Supprime le gameObject
+        Destroy (gameObject);
+        //Supprime l'isntance du script utilisé
+        Destroy(this);
+    }
+}
