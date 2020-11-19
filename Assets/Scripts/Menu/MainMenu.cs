ef9f0dc3708e0344d5792801c2b8625ca7edb578
GEYER CLEMENT <clement.geyer@etu.unistra.fr>
2020-11-19 19:41:43 +0100

    Changements menu + ajout menu pause

--- Assets/Scripts/Menu/MainMenu.cs
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -1,21 +1,28 @@
-﻿using UnityEngine;
+﻿using UnityEditor;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
-    public void StartGame()
+
+    public GameObject menu;
+    
+    //Méthode qui permet de charger la scène de sélection de niveaux quand on appuie sur le bouton "Histoire"
+    public void LoadLevelSelection()
     {
         SceneManager.LoadScene(1);
     }
 
+    //Méthode qui permet de quitter le jeu quand on appuie sur le bouton "Quit"
     public void QuitGame()
     {
         Application.Quit();
     }
 
-    public void OptionsMenu()
+    //Méthode qui permet d'afficher le menu des options
+    public void LoadOptionsMenu()
     {
-        SceneManager.LoadScene(2);
+        menu.SetActive(true);
     }
 }
