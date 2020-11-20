9aa8fdd5c42637acedad3c4f4a442ce792d05e98
HAQUIN ELOUAN <elouan.haquin@etu.unistra.fr>
2020-11-20 16:32:07 +0100

    Changement des scripts de camera

--- Assets/Scripts/Controller/CameraMovements.cs
+++ Assets/Scripts/Controller/CameraMovements.cs
@@ -7,38 +7,47 @@ namespace Controller
         [Header("Camera parameters")]
         [SerializeField] private float offsetCamera = 0.0f;
         [SerializeField] private float speedCamera = 0.05f;
-        [SerializeField] private float increaseSpeedCamera = 0.05f;
+        [SerializeField] public float increaseSpeedCamera = 0.05f;
+         public bool shouldMove;
+         public float increaseSpeedCameraBuff = 0.0f;
         public float maxspeed; 
+
+       
     
         public GameObject hero;
         private Transform tf;
 
+        public void setShouldMove(bool p_b){
+            shouldMove = p_b;
+        }
         void Start()
         {
+            increaseSpeedCameraBuff = increaseSpeedCamera;
+            shouldMove = true;
             tf = this.GetComponent<Transform>();
         }
 
-   
+    
         void FixedUpdate()
         {
-            // Seuil de vitesse max
-            if(speedCamera <= maxspeed)
-                speedCamera *= 1+ increaseSpeedCamera;
-        
-            // Met à jour la position de la caméra tout en suivant le joueur sur l'axe y
-            tf.position = new Vector3(  tf.position.x + speedCamera, tf.position.y,  tf.position.z);
+            if(shouldMove){
+                // Seuil de vitesse max
+                if(speedCamera <= maxspeed)
+                    speedCamera *= 1+ increaseSpeedCamera;
+            
+                // Met à jour la position de la caméra tout en suivant le joueur sur l'axe y
+                tf.position = new Vector3(  tf.position.x + speedCamera, tf.position.y,  tf.position.z);
 
-            // Empêche la caméra de revenir en arrière
-            if( tf.position.x < hero.GetComponent<Transform>().position.x )
-                tf.position = new Vector3( hero.GetComponent<Transform>().position.x, tf.position.y,  tf.position.z);
+                // Empêche la caméra de revenir en arrière
+                if( tf.position.x < hero.GetComponent<Transform>().position.x )
+                    tf.position = new Vector3( hero.GetComponent<Transform>().position.x, tf.position.y,  tf.position.z);
 
 
-            //Pour que la camera suive completement le joueur (pour les tests)
-            /* tf.position = new Vector3( hero.GetComponent<Transform>().position.x, tf.position.y,  tf.position.z);
-         
-         tf.position = new Vector3(tf.position.x , hero.GetComponent<Transform>().position.y,  tf.position.z);*/
-        
-        
+                //Pour que la camera suive completement le joueur (pour les tests)
+                /* tf.position = new Vector3( hero.GetComponent<Transform>().position.x, tf.position.y,  tf.position.z);
+            
+            tf.position = new Vector3(tf.position.x , hero.GetComponent<Transform>().position.y,  tf.position.z);*/
+            }
         }
     }
 }
