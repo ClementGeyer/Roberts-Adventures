05526ea27ec0d7f7765f6dd91cd2639d83c748a4
EDEL GAUTIER <gautier.edel@etu.unistra.fr>
2020-12-10 10:55:03 +0100

    Ajout d'un avertissement avant de rencontrer un projectile

---
+++ Assets/Scripts/Map/Projectiles/ProjectileWarning.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using Random = UnityEngine.Random;
+
+public class ProjectileWarning : MonoBehaviour
+{
+    public Image warning;
+    public float onScreenTime;
+    public float minFlickerSpeed = 0.1f;
+    public float maxFlickerSpeed = 1.0f;
+    
+    /// <summary>
+    /// Start est appelé avant que la première frame se mette à jour
+    /// </summary>
+    void Start()
+    {
+        // Supprime le warning après un certain temps
+        Destroy(warning, onScreenTime);
+    }
+
+    /// <summary>
+    /// Update est appelé à chaque frame
+    /// </summary>
+    private void Update()
+    {
+        if (warning != null)
+        {
+            // Fait clignoter le warning
+            var color = warning.color;
+            warning.color = new Color(color.r, color.g, color.b, Random.Range(minFlickerSpeed, maxFlickerSpeed));
+        }
+    }
+}
