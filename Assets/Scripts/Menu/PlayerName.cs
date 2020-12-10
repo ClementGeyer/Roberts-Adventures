b166c1e307401c1208e16b6fb0fb9db33b150833
GEYER CLEMENT <clement.geyer@etu.unistra.fr>
2020-12-10 18:00:13 +0100

    Ajout de la demande du nom du joueur

---
+++ Assets/Scripts/Menu/PlayerName.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Net.Mime;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Menu
+{
+    public class PlayerName : MonoBehaviour
+    {
+        public GameObject playerNameUI;
+        public InputField inputName;
+        public static string playerName;
+        
+        /// <summary>
+        /// Quand on clique sur le bouton on récupère la valeur du nom du joueur
+        /// </summary>
+        public void clic()
+        {
+            // On accepte pas le clic si le joueur ne rempli rien
+            if (inputName.text != "")
+            {
+                playerName = inputName.text;
+                Debug.Log(playerName);
+                playerNameUI.SetActive(false);
+            }
+        }
+    }
+}
+
